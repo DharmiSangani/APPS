ebff9016339e8ac1af8d598c29c4cfcdbb5229ce
DharmiSangani <dharmisangani21@gmail.com>
2025-01-03 15:43:33 +0530

    apply chnages

--- First/models.py
+++ First/models.py
@@ -9,5 +9,6 @@ class Task(models.Model):
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
 def __str__(self):
     return self.title
--- First/urls.py
+++ First/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),          # Task list view
     path('create/', views.task_create, name='task_create'),  # Create task view
     path('update/<int:task_id>/', views.task_update, name='task_update'),  # Update task view
-    path('delete/<int:task_id>/', views.task_delete, name='task_delete'), # Delete task view
+    path('delete/<int:task_id>/', views.task_delete, name='task_delete'),  # Delete task view
 ]
 
